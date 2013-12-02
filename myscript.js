1e282b32d7ca0fc08b6023948dd520a000b4dc7e
coolbananasray <raydeljessonprado@gmail.com>
2013-12-02 22:44:42 +1100

    Copied files from home laptop to Repository

--- myscript.js
+++ myscript.js
@@ -1,1 +1,11 @@
-console.log("Hello World");
+console.log("Hello from myscript.js"); //example of a console prompt
+console.log("Hello again!");
+
+var name = prompt("What is your name?");
+alert("Hello " + name); 
+
+name = "Nick"; // reassign the name variable to Nick
+
+console.log("the user's name is " + name);
+// EXAMPLE: alert("Hello " + prompt("What is your name?")); this command 1st prompts the inner parameter "prompt", then the outside "alert".
+
